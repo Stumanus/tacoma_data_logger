4583523c4ed90b7b36a1d31d552dafdae127e1f1
Stu <stuart.c.frost@gmail.com>
2025-03-06 20:40:38 -0700

    discovered udev rule need

--- battery_logger.py
+++ battery_logger.py
@@ -17,71 +17,64 @@ logger.setLevel(logging.INFO)
 the whole program doesn't hang'''
 
 try:
-    controller = EpeverChargeController('/dev/ttyUSB0',1)
+    controller = EpeverChargeController('/dev/ttyUSB1',1)
 except Exception as e:
     logger.info(f'Error: {e}. Problem initializing connection with charge controller...exiting.')
     exit()
 
-data_logging_interval = 60
+try:
+    data = {
+        'datetime' : str(datetime.now()),
+        #Stats:
+        'solar_voltage_V' : f'{controller.get_solar_voltage()}',
+        'solar_current_A' : f'{controller.get_solar_current()}',
+        'solar_power_W' : f'{controller.get_solar_power()}',
+        'load_voltage_V' : f'{controller.get_load_voltage()}',
+        'load_current_A' : f'{controller.get_load_current()}',
+        'load_power_W' : f'{controller.get_load_power()}',
+        'battery_voltage_V' : f'{controller.get_battery_voltage()}',
+        'battery_current_A' : f'{controller.get_battery_current()}',
+        'battery_power_W' : f'{controller.get_battery_power()}',
+        'battery_soc_%' : f'{controller.get_battery_state_of_charge()}'
+    }
+    battery_status = controller.get_battery_status()
+    equip_data = {
+        'current_device_time' : f'{controller.get_rtc()}',
+        'device_overtemp_status' : f'{controller.is_device_over_temperature()}',
+        #Battery Status
+        'wrong_id_for_rated_voltage' : battery_status['wrong_identifaction_for_rated_voltage'],
+        'battery_inner_resistance_abnormal' : battery_status['battery_inner_resistence_abnormal'],
+        'temperature_warning_status' : battery_status['temperature_warning_status'],
+        'battery_status' : battery_status['battery_status']
+    }
+except Exception as e:
+    logger.info(f'Problem reading battery data into dict: {e}')
 
-def main():
-    while True:
-        time.sleep(data_logging_interval)
-        try:
-            data = {
-                    'datetime' : str(datetime.now()),
-                    #Stats:
-                    'solar_voltage_V' : f'{controller.get_solar_voltage()}',
-                    'solar_current_A' : f'{controller.get_solar_current()}',
-                    'solar_power_W' : f'{controller.get_solar_power()}',
-                    'load_voltage_V' : f'{controller.get_load_voltage()}',
-                    'load_current_A' : f'{controller.get_load_current()}',
-                    'load_power_W' : f'{controller.get_load_power()}',
-                    'battery_voltage_V' : f'{controller.get_battery_voltage()}',
-                    'battery_current_A' : f'{controller.get_battery_current()}',
-                    'battery_power_W' : f'{controller.get_battery_power()}',
-                    'battery_soc_%' : f'{controller.get_battery_state_of_charge()}'
-            }
-            battery_status = controller.get_battery_status()
-            equip_data = {
-                    'current_device_time' : f'{controller.get_rtc()}',
-                    'device_overtemp_status' : f'{controller.is_device_over_temperature()}',
-                    #Battery Status
-                    'wrong_id_for_rated_voltage' : battery_status['wrong_identifaction_for_rated_voltage'],
-                    'battery_inner_resistance_abnormal' : battery_status['battery_inner_resistence_abnormal'],
-                    'temperature_warning_status' : battery_status['temperature_warning_status'],
-                    'battery_status' : battery_status['battery_status']
-            }
-        except Exception as e:
-            logger.info(f'Problem reading battery data into dict: {e}')
+try:
+    data_tuple = tuple(list(data.values()))
+    column_tuple = tuple(list(data.keys()))
 
-        try:
-            data_tuple = tuple(list(data.values()))
-            column_tuple = tuple(list(data.keys()))
+    equip_data_tuple = tuple(list(equip_data.values()))
+    equip_column_tuple = tuple(list(equip_data.keys()))
 
-            equip_data_tuple = tuple(list(equip_data.values()))
-            equip_column_tuple = tuple(list(equip_data.keys()))
+    conn = sqlite3.connect('/home/stu/tacoma_data_logger/battery.db')
+    cur = conn.cursor()
 
-            conn = sqlite3.connect('/home/stu/tacoma_data_logger/battery.db')
-            cur = conn.cursor()
+    tables = [x[0] for x in cur.execute('SELECT name FROM sqlite_master').fetchall()]
+    if 'battery' not in tables:
+        cur.execute(f'CREATE TABLE battery{column_tuple}')
+    if 'status' not in tables:
+        cur.execute(f'CREATE TABLE status{equip_column_tuple}')
 
-            tables = [x[0] for x in cur.execute('SELECT name FROM sqlite_master').fetchall()]
-            if 'battery' not in tables:
-                cur.execute(f'CREATE TABLE battery{column_tuple}')
-            if 'status' not in tables:
-                cur.execute(f'CREATE TABLE status{equip_column_tuple}')
+    placeholders = ('?, ' * len(data))[:-2]
+    cur.execute(f'INSERT INTO battery VALUES({placeholders})',data_tuple)
 
-            placeholders = ('?, ' * len(data))[:-2]
-            cur.execute(f'INSERT INTO battery VALUES({placeholders})',data_tuple)
+    placeholders = ('?, ' * len(equip_data))[:-2]
+    cur.execute(f'INSERT INTO status VALUES({placeholders})',equip_data_tuple)
 
-            placeholders = ('?, ' * len(equip_data))[:-2]
-            cur.execute(f'INSERT INTO status VALUES({placeholders})',equip_data_tuple)
+    conn.commit()
+    conn.close()
+    logger.info(f'DB entry successful')
+except Exception as e:
+    logger.info(f'Problem inserting data into database: {e}')
 
-            conn.commit()
-            conn.close()
-            logger.info(f'DB entry successful')
-        except Exception as e:
-            logger.info(f'Problem inserting data into database: {e}')
-
-if __name__ == '__main__':
-    main()
