0df15f9e97e7a70f630ea9ac5cf112c5ae224514
Stu <stuart.c.frost@gmail.com>
2025-03-14 19:50:00 -0700

    created script to ping tacoma and sync if reachable

--- syncdb.sh
+++ syncdb.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 REMOTE_HOST="192.168.0.139"
-REMOTE_DB_PATH=""
-LOCAL_DB_PATH=""
-LOG_FILE=""
+REMOTE_DB_PATH="/home/stu/tacoma_data_logger/battery.db"
+LOCAL_DB_PATH="/home/stu/battery.db"
+LOG_FILE="battery.log"
 
-ping -c 1 "$REMOTE_HOST" $>/dev/null
+ping -c 1 "$REMOTE_HOST" &>/dev/null
 
 if [ $? -eq 0 ]; then
-  echo "$(date): tacoma detected, syncing db" >> LOG_FILE
+  echo "$(date): tacoma detected, syncing db" >> $LOG_FILE
   
   rsync -avz --delete "$REMOTE_HOST:$REMOTE_DB_PATH" "$LOCAL_DB_PATH" >> "$LOG_FILE" 2>&1
   
